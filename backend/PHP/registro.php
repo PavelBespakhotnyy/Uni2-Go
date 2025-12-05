1fc82e748b9977366e14bebd7347de2e68a75d56
Eric Ibanez <IM2025129@imesmaresme.com>
2025-12-05 10:17:06 +0100

    Add Extra PHP

---
+++ backend/PHP/registro.php
@@ -0,0 +1,10 @@
+<?php 
+require 'index.php';
+if (isset($_POST['enviar'] && $_POST['enviar'] == "Registrarse")) {
+    if (!empty($_POST['nombre']) && !empty($_POST['apellido'])) {
+}
+}
+$sql = "INSERT INTO `users`()"
+
+
+?>
