0f22d61083f34892ea6cc6bf6f246ab6e7bdf143
Eric Ibanez <IM2025129@imesmaresme.com>
2025-12-09 11:55:59 +0100

    Registro a la base de datos

--- backend/PHP/registro.php
+++ backend/PHP/registro.php
@@ -9,15 +9,12 @@ if (isset($_POST['enviar']) && $_POST['enviar'] == 'Registrarse') {
         $correo = $_POST['Correo'];
         $telefono = $_POST['Telefono'];
         $contrasena = $_POST['contrasena'];
-        print_r($nombre);
-        print_r($apellido);
-        print_r($nacimiento);
-        print_r($correo);
-        print_r($telefono);
-        print_r($contrasena);
-        /*$sql = "INSERT INTO `users`(`email`, `password_hash`, `first_name`, `last_name`, `phone`, `date_of_birth`, `created_at`, `updated_at`, `is_active`) VALUES ('$correo','$contrasena','$nombre','$apellido','$telefono','$nacimiento','[value-7]','[value-8]','[value-9]')";
+        $fecha_creacion = date('Y-m-d H:i:s');
+        $fecha_actualizado = $fecha_creacion; 
+        $activo = isset($_POST['activo']) ? 1 : 0;
+        $sql = "INSERT INTO `users`(`email`, `password_hash`, `first_name`, `last_name`, `phone`, `date_of_birth`, `created_at`, `updated_at`, `is_active`) VALUES ('$correo','$contrasena','$nombre','$apellido','$telefono','$nacimiento','$fecha_creacion','$fecha_actualizado','$activo')";
         $resultado = $con->query($sql);
-        */
+        
 ?>
         <script>
             window.location.href = '/Uni2-Go/frontend/pages/login.html';
