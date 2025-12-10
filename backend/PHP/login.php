10a29900d17410d40319b01a23def8ebdb412b54
Eric Ibanez <IM2025129@imesmaresme.com>
2025-12-10 08:19:14 +0100

    funcionamiento del login.html y verificacion en login.php

--- backend/PHP/login.php
+++ backend/PHP/login.php
@@ -1,5 +1,6 @@
 <?php 
 require 'index.php';
+$login_page = '/Uni2-Go/frontend/pages/login.html';
 if (isset($_POST['enviar']) && $_POST['enviar'] == 'Iniciar_Sesion') {
     if (!empty($_POST['correoUsuario']) && !empty($_POST['contrasena'])) {
         $correo = $_POST['correoUsuario'];
@@ -7,7 +8,11 @@ if (isset($_POST['enviar']) && $_POST['enviar'] == 'Iniciar_Sesion') {
         $sql = "SELECT `email`, `password_hash` FROM users WHERE email = '$correo'  AND password_hash = '$contrasena'";
         $result = $con->query($sql);
         $row = $result->fetch_assoc();
-        if ($row) {
+        
+        if ($row === null) {
+            header("Location: $login_page?error=db_error");
+            exit();
+        } else{
             ?>
             <script>
                 window.location.href = '/Uni2-Go/frontend/pages/calendario.html';
