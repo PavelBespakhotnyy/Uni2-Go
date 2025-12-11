86b7aa82375f3adcefd4d468b99ca73ce7e88126
Eric Ibanez <IM2025129@imesmaresme.com>
2025-12-11 13:51:28 +0100

    Mejora en el registro, implementar el password_hash, verificacion en el login

--- backend/PHP/login.php
+++ backend/PHP/login.php
@@ -5,17 +5,24 @@ if (isset($_POST['enviar']) && $_POST['enviar'] == 'Iniciar_Sesion') {
     if (!empty($_POST['correoUsuario']) && !empty($_POST['contrasena'])) {
         $correo = $_POST['correoUsuario'];
         $contrasena = $_POST['contrasena'];
+        $fecha_actualizada = date('Y-m-d H:i:s');
         $sql = "SELECT `email`, `password_hash` FROM users WHERE email = '$correo'  AND password_hash = '$contrasena'";
         $result = $con->query($sql);
         $row = $result->fetch_assoc();
         
-        if ($row === null) {
-            header("Location: $login_page?error=db_error");
-            exit();
-        } else{
+        if (password_verify($contrasena, $row['password_hash'])) {
             ?>
             <script>
-                window.location.href = '/Uni2-Go/frontend/pages/calendario.html';
+                alert('Error: Las contraseña o el correo no coincide. Por favor, verifícala.'); 
+                window.location.href = '/Uni2-Go/frontend/pages/login.html';
+            </script>
+            <?php
+        } else{
+            $sql2 = "UPDATE `users` SET `updated_at`='$fecha_actualizada' WHERE email = '$correo'  AND password_hash = '$contrasena'";
+            $result2 = $con->query($sql2);
+            ?>
+            <script>
+                window.location.href = '/Uni2-Go/frontend/pages/calendario.html?correo=<?php echo $correo;?>';
             </script>
             <?php
         }
--- backend/PHP/registro.php
+++ backend/PHP/registro.php
@@ -9,10 +9,12 @@ if (isset($_POST['enviar']) && $_POST['enviar'] == 'Registrarse') {
         $correo = $_POST['Correo'];
         $telefono = $_POST['Telefono'];
         $contrasena = $_POST['contrasena'];
+        // --- PASO CLAVE: GENERAR EL HASH ---
+        $contrasena_hash = password_hash($contrasena, PASSWORD_DEFAULT);
         $fecha_creacion = date('Y-m-d H:i:s');
         $fecha_actualizado = $fecha_creacion; 
         $activo = isset($_POST['activo']) ? 1 : 0;
-        $sql = "INSERT INTO `users`(`email`, `password_hash`, `first_name`, `last_name`, `phone`, `date_of_birth`, `created_at`, `updated_at`, `is_active`) VALUES ('$correo','$contrasena','$nombre','$apellido','$telefono','$nacimiento','$fecha_creacion','$fecha_actualizado','$activo')";
+        $sql = "INSERT INTO `users`(`email`, `password_hash`, `first_name`, `last_name`, `phone`, `date_of_birth`, `created_at`, `updated_at`, `is_active`) VALUES ('$correo','$contrasena_hash','$nombre','$apellido','$telefono','$nacimiento','$fecha_creacion','$fecha_actualizado','$activo')";
         $resultado = $con->query($sql);
         
 ?>
