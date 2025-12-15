39d40d984746c58feadd2b17d491ca12c496fe58
Eric Ibanez <IM2025129@imesmaresme.com>
2025-12-15 11:14:56 +0100

    cambios de envio

--- backend/PHP/login.php
+++ backend/PHP/login.php
@@ -6,23 +6,23 @@ if (isset($_POST['enviar']) && $_POST['enviar'] == 'Iniciar_Sesion') {
         $correo = $_POST['correoUsuario'];
         $contrasena = $_POST['contrasena'];
         $fecha_actualizada = date('Y-m-d H:i:s');
-        $sql = "SELECT `email`, `password_hash` FROM users WHERE email = '$correo'  AND password_hash = '$contrasena'";
+        $sql = "SELECT `email`, `password_hash` FROM users WHERE email = '$correo'";
         $result = $con->query($sql);
         $row = $result->fetch_assoc();
         
         if (password_verify($contrasena, $row['password_hash'])) {
-            ?>
-            <script>
-                alert('Error: Las contraseña o el correo no coincide. Por favor, verifícala.'); 
-                window.location.href = '/Uni2-Go/frontend/pages/login.html';
-            </script>
-            <?php
-        } else{
             $sql2 = "UPDATE `users` SET `updated_at`='$fecha_actualizada' WHERE email = '$correo'";
             $result2 = $con->query($sql2);
             ?>
             <script>
-                window.location.href = '/Uni2-Go/frontend/pages/calendario.html?correo=<?php echo $correo;?>';
+                window.location.href = '/Uni2-Go/frontend/pages/calendario.html';
+            </script>
+            <?php
+        } else{
+            ?>
+            <script>
+                alert('Error: Las contraseña o el correo no coincide. Por favor, verifícala.'); 
+                window.location.href = '/Uni2-Go/frontend/pages/login.html';
             </script>
             <?php
         }
