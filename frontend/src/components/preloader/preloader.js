d551b4e521f7dcb58c8253b63693a8571f848c2f
Pavel Bespakhotnyi <pavel.bespakhotnyy@gmail.com>
2026-02-11 08:41:12 +0100

    Fixed infinite preloder in npm run build

--- frontend/src/components/preloader/preloader.js
+++ frontend/src/components/preloader/preloader.js
@@ -1,18 +1,6 @@
-// function initPreloader() {
-//   const preloader = document.getElementById('preloader');
-//   const style = document.getElementById('main-style');
-
-//   if (!preloader || !style) return;
-
-//   const hidePreloader = () => {
-//     preloader.style.display = 'none';
-//   };
-
-//   if (style.sheet) hidePreloader();
-//   else {
-//     style.addEventListener('load', hidePreloader, { once: true });
-//     style.addEventListener('error', hidePreloader, { once: true });
-//   }
-// }
-
-// document.addEventListener('DOMContentLoaded', initPreloader);
+window.addEventListener('load', () => {
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    preloader.style.display = 'none';
+  }
+});
