ead0eae6351273475d2a1b97f01f5e4fb46e672c
Pavel Bespakhotnyi <pavel.bespakhotnyy@gmail.com>
2026-02-11 08:26:16 +0100

    Fixed pages not switching in npm run build/preview

--- frontend/src/components/preloader/preloader.js
+++ frontend/src/components/preloader/preloader.js
@@ -1,18 +1,18 @@
-function initPreloader() {
-  const preloader = document.getElementById('preloader');
-  const style = document.getElementById('main-style');
+// function initPreloader() {
+//   const preloader = document.getElementById('preloader');
+//   const style = document.getElementById('main-style');
 
-  if (!preloader || !style) return;
+//   if (!preloader || !style) return;
 
-  const hidePreloader = () => {
-    preloader.style.display = 'none';
-  };
+//   const hidePreloader = () => {
+//     preloader.style.display = 'none';
+//   };
 
-  if (style.sheet) hidePreloader();
-  else {
-    style.addEventListener('load', hidePreloader, { once: true });
-    style.addEventListener('error', hidePreloader, { once: true });
-  }
-}
+//   if (style.sheet) hidePreloader();
+//   else {
+//     style.addEventListener('load', hidePreloader, { once: true });
+//     style.addEventListener('error', hidePreloader, { once: true });
+//   }
+// }
 
-document.addEventListener('DOMContentLoaded', initPreloader);
+// document.addEventListener('DOMContentLoaded', initPreloader);
